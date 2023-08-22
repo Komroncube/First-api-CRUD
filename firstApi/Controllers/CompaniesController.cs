0242b46f233cd2afe7be752d35c1ce0cdc595b42
Komron <komronabdujabbor@gmail.com>
2023-08-22 19:13:09 +0500

    major changes of api

---
+++ firstApi/Controllers/CompaniesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Dtos;
+using Service.Interfaces;
+
+namespace firstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+        public CompaniesController(ICompanyRepository companyRepository)
+        {
+            this._companyRepository = companyRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCompany([FromForm] CreateCompanyDto createCompanyDto)
+        {
+            await _companyRepository.CreateCompanyAsync(createCompanyDto);
+            return Ok("Created");
+        }
+    }
+}
--- firstApi/Program.cs
+++ firstApi/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Service.DataContext;
+using Service.Interfaces;
+using Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("PostgresqlConnection")
+    ));
 
+builder.Services.AddTransient<ICompanyRepository, CompanyRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
