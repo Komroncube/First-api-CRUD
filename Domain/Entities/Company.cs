776aa270edd01697b8e5d19e750a8430fc597f87
Komron <komronabdujabbor@gmail.com>
2023-08-22 19:09:45 +0500

    main models of this project

---
+++ Domain/Entities/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+        public ICollection<Staff> Staffs { get; set; }
+    }
+}
