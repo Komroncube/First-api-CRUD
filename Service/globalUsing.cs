6e056029e2d3c9cfa83f6d84aee11c28e8072159
Komron <komronabdujabbor@gmail.com>
2023-08-23 21:10:27 +0500

    global using created

---
+++ Service/globalUsing.cs
@@ -0,0 +1,13 @@
+﻿
+global using Domain.Entities;
+global using Service.Dtos.Employees;
+global using Service.Dtos.Companies;
+global using Microsoft.EntityFrameworkCore;
+global using Service.DataContext;
+global using Service.Interfaces;
+global using Microsoft.EntityFrameworkCore;
+global using Service.DataContext;
+global using Service.Interfaces;
+global using Service.Services;
+global using System.Net;
+using System;
