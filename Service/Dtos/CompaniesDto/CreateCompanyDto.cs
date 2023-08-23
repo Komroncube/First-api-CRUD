32d9d437891b470d976e0c72e7c167eb0dfe3ccf
Komron <komronabdujabbor@gmail.com>
2023-08-23 21:04:45 +0500

    CompanyDto created

---
+++ Service/Dtos/CompaniesDto/CreateCompanyDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Dtos.Companies
+{
+    public class CreateCompanyDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Service/Dtos/CompaniesDto/ResultCompanyDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Service.Dtos.Companies
+{
+    public class ResultCompanyDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
