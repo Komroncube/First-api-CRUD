3e504f03b459b65e9a43fc526f1a07f2ac6472ad
Komron <komronabdujabbor@gmail.com>
2023-08-23 13:54:43 +0500

    added contstructor to map Company to createCompanyDto

--- Service/Dtos/CreateCompanyDto.cs
+++ Service/Dtos/CreateCompanyDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,18 @@ namespace Service.Dtos
 {
     public class CreateCompanyDto
     {
+        public CreateCompanyDto() { }
+        /// <summary>
+        /// maps Company class to CreateCompanyDto
+        /// </summary>
+        /// <param name="company"></param>
+        public CreateCompanyDto(Company company)
+        {
+            Name=company.Name;
+            Address=company.Address;
+            Phone=company.Phone;
+            Email=company.Email;
+        }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
