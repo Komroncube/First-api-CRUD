ce8bb4f061e3ff4413243503b78d7f4c6e53d188
Komron <komronabdujabbor@gmail.com>
2023-08-30 17:58:32 +0500

    these are custom middlewares

---
+++ firstApi/CustomMiddlewares/CheckTokenMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace firstApi.CustomMiddlewares
+{
+    public class CheckTokenMiddleware
+    {
+        private RequestDelegate next { get; }
+
+        public CheckTokenMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+            if (token != "123456")
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+            {
+                await next.Invoke(context);
+            }
+        }
+    }
+}
